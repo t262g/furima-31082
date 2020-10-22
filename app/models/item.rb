44293fb78a61f4aab04f98fcae501fa703aa1b0b
t262g <t262gkyoutarou@gmail.com>
2020-10-22 22:28:31 +0900

    Itemモデルのアソシエーションとバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,31 @@
 class Item < ApplicationRecord
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :area
+  belongs_to_active_hash :delivery_day
+
+  belongs_to :user
+  has_one_attached :image
+
+  with_options numericality: {other_than: 1} do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :area_id
+    validates :delivery_day_id
+  end
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :area_id
+    validates :delivery_day_id
+  end
+
 end
