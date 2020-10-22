6718b74b86fa855b1e1e47929edc555c71666f89
t262g <t262gkyoutarou@gmail.com>
2020-10-22 23:40:30 +0900

    Itemsコントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :price, :explanation, :category_id, :condition_id, :shipping_cost_id, :area_id, :delivery_day_id).merge(user_id: current_user.id)
   end
 end
