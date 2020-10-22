b2a190a1f26f5fe3c0b5f2cfe60745dab3610f9f
t262g <t262gkyoutarou@gmail.com>
2020-10-22 14:30:15 +0900

    バリデーションの記述の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :birthday, presence: true
+
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
