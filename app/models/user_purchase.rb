e00d2edbf6a12eec8c7c9d9d2f3690aae20ec2e3
t262g <t262gkyoutarou@gmail.com>
2020-10-27 17:41:27 +0900

    フォームオブジェクトの修正

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,12 +1,13 @@
 class UserPurchase
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :area_id, :city, :address_line_1, :address_line_2, :phone_number
+  attr_accessor :token, :price, :user_id, :item_id, :postal_code, :area_id, :city, :address_line_1, :address_line_2, :phone_number
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address_line_1
     validates :phone_number, format: {with: /\d{,11}/}
+    validates :token
   end
 
   validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
