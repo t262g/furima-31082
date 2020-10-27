81067f97a8c47ee87ed87fa0c2c019073a32e9bb
t262g <t262gkyoutarou@gmail.com>
2020-10-27 11:54:49 +0900

    未購入の商品のみを出品者以外のログイン済みユーザーのみが購入できる記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,14 @@
 class PurchasesController < ApplicationController
+  before_action :find_item, only: [:index, :create]
+
   def index
-    find_item
+    check_purchase
+    authenticate_user!
+    identify_user
     @user_purchase = UserPurchase.new
   end
 
   def create
-    find_item
     @user_purchase = UserPurchase.new(purchase_params)
     if @user_purchase.valid?
       @user_purchase.save
@@ -21,7 +24,20 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def check_purchase
+    if @item.purchase != nil
+      redirect_to root_path
+    end
+  end
+
+  def identify_user
+    if current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
   def purchase_params
     params.require(:user_purchase).permit(:postal_code, :area_id, :city, :address_line_1, :address_line_2, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
+
 end
