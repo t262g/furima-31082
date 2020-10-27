ad0acfea43f7f5b7de13aaa8a4051f968f47d7e0
t262g <t262gkyoutarou@gmail.com>
2020-10-27 11:02:31 +0900

    Purchasesコントローラーの記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,27 @@
 class PurchasesController < ApplicationController
   def index
-    @item = Item.find(params[:item_id])
+    find_item
+    @user_purchase = UserPurchase.new
   end
 
   def create
+    find_item
+    @user_purchase = UserPurchase.new(purchase_params)
+    if @user_purchase.valid?
+      @user_purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private 
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def purchase_params
+    params.require(:user_purchase).permit(:postal_code, :area_id, :city, :address_line_1, :address_line_2, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
